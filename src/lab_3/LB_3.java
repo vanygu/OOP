aa666ad9f51c998edb5a11b5546c540441dd07a0
vanygu <vanygu21@gmail.com>
2026-05-05 18:58:57 +0100

    finish lb-3

--- src/lab_3/LB_3.java
+++ src/lab_3/LB_3.java
@@ -1,4 +1,22 @@
 package lab_3;
 
-public class lb {
+public class LB_3 {
+    public static void main(String[] args) {
+
+        int[] ar = {10, -8, -65, -6, -14, 0};
+
+        int sum = 0;
+
+
+        for (int i = 0; i < ar.length; i++) {
+
+
+            if (ar[i] < 0 && ar[i] % 2 == 0) {
+                sum = sum + ar[i];
+            }
+        }
+
+
+        System.out.println("Сума від'ємних парних елементів: " + sum);
+    }
 }
